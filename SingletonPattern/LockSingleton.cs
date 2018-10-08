b7d1382e0323424558a00ea59b063b02f0886ef4
Iftekhar Rasul <iftekhar.rasul7@gmail.com>
2018-10-08 16:37:22 +0600

    SingletonPattern

---
+++ SingletonPattern/LockSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class LockSingleton
+    {
+        private static LockSingleton instance = null;
+        private static readonly object padlock = new object();
+
+        LockSingleton()
+        {
+        }
+
+        public static LockSingleton InstanceGet()
+        {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LockSingleton();
+                    }
+                    return instance;
+                }
+        }
+    }
+}
---
+++ SingletonPattern/SimpleSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class SimpleSingleton // not thread-safe
+    {
+        private static SimpleSingleton instance = null;
+
+        private SimpleSingleton()
+        {
+        }
+
+        public static SimpleSingleton GetInstance()
+        {
+                if (instance == null)
+                {
+                    instance = new SimpleSingleton();
+                }
+                return instance;
+        }
+    }
+}
